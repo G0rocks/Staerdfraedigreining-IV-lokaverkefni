1601197227cc4a5ab3e38d0b58bd31b5845c0c29
doramagnea <dmh8@hi.is>
2021-03-30 15:35:32 +0000

    bæta við prófunarkeyrslu Fixed #5

--- main.m
+++ main.m
@@ -1,10 +1,18 @@
 % Fyrri hluti - Helmholtz
 
 % lidur 1
+    %Prófunarkeyrsla
+lambda = 1/100; L1 = 1; L2 = 1; h = 1/4;
+for x = 0:1
+    w = 1;
+    v = 0;
+end
 
 
+helmholtzeq(L1, L2, h, lambda)git push
 
 % lidur 2
+
 
 
 
