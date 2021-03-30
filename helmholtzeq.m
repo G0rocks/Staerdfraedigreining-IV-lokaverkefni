d0b06e11366585800d86d9b6804ee049ed8761fd
G0rocks <huldarh@gmail.com>
2021-03-30 15:32:28 +0000

    Helmholtzeq.m tilbúið
    Fixes #4

--- helmholtzeq.m
+++ helmholtzeq.m
@@ -1,8 +1,18 @@
-%   
-
+% Fall sem tekur inn L1, L2,h og lambda og skilar Helmholtz fylkinu HZ
+% 
 function HZ = helmholtzeq(L1, L2, h, lambda)
+  % 
+  N = L1/h;
+  M = L2/h;
   
-  HZ = [1,2;3,4];
-  
-  
-end
+  % Initialize HZ array with zeros
+  HZ = zeros(M+1 , N+1)
+
+  for i = 1:(M+1)   % Rows
+    y = (i-1).*h
+    for j = 1:(N+1) % Columns
+      x = (j-1).*h
+      HZ(i,j) = thrihyrningsnalgun(x,y)
+    end    % Column for loop
+  end  % Row for loop
+end % Function
