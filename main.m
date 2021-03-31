b537be64e6a3c85e77fa90ada0fc5608dffbab11
doramagnea <dmh8@hi.is>
2021-03-31 12:40:45 +0000

    Laga prufukeyrslu og bæta við HZ_lausn fylki

--- main.m
+++ main.m
@@ -2,14 +2,15 @@
 
 % lidur 1
     %Prófunarkeyrsla
-lambda = 1/100; L1 = 1; L2 = 1; h = 1/4;
-for x = 0:1
-    w = 1;
-    v = 0;
-end
+lambda = 1/100; L1 = 1; L2 = 1; h = 1/4; w = 1; v = 0;
+
+helmholtzeq(L1, L2, h, lambda)
 
 
-helmholtzeq(L1, L2, h, lambda)git push
+HZ_lausn = [1. 1. 1. 1. 1.; .75 .75 .75 .75 .75; .50 .50 .50 .50 .50; .25 .25 .25 .25 .25; 0. 0. 0. 0. 0.]
+
+
+disp(HZ_lausn)
 
 % lidur 2
 
