a235f85e7eed50002fed62c108ae7a9f7f163256
Ragnar Tómasson <ragnartomasson@Ragnars-MacBook-Pro-2.local>
2021-04-01 13:17:48 +0000

    testar að psi forritin virki

---
+++ varmajafnvaegi.m
@@ -0,0 +1,9 @@
+%forritið fyrir psí verður bara í raun að leysa upphafsgildisverkefnið og
+%svo vera með tvær for loopur sem prenta út í fylki
+
+
+%testa að psi forritin virki
+Beta1=1; a=1; x=1;Beta2=1;
+psi_1=psi1(a, Beta1, x)
+
+psi_2 = psi2(Beta2,x)
