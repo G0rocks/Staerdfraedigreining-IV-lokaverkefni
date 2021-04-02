e67a3e16b0f3f4592c46e4cbc86b08c76ddd2e56
Huldar <huldarh@gmail.com>
2021-04-02 12:33:35 +0000

    Fleiri inntök
    Bæta við inntökum

--- helmholtzNalgun.m
+++ helmholtzNalgun.m
@@ -1,7 +1,9 @@
 % Function sem tekur inn x og y hnit á 
 
-function output = helmholtzNalgun(A,y)
+function output = helmholtzNalgun(x,y, L1, L2, lambda, N, M, h);
   % Inverse quadratic interpolation
+  
+  % Hér þarf að nota mismunaaðferðina
   
   
   output = 1;
