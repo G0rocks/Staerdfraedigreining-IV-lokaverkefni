d462eb9c2f600032a1ea044f88c1a04df5d1fd5c
Huldar <huldarh@gmail.com>
2021-04-02 12:22:57 +0000

    Útfæra v_HZ og w_HZ föll
    Fixes #10 og #11

---
+++ v_HZ.m
@@ -0,0 +1,11 @@
+% v function
+
+function v = v_HZ(x)
+  if (x >= 0 && x <= 1)
+    v = 0;
+    return;
+  end % if
+  
+  v = NaN;
+  disp(strcat("v_HZ Error: Can not compute v with x = ", num2str(x)));
+end   % Function
---
+++ w_HZ.m
@@ -0,0 +1,1 @@
+% w functionfunction w = w_HZ(x)  if (x >= 0 && x <= 1)    w = 1;    return;  end % if    v = NaN;  disp(strcat("w_HZ Error: Can not compute w with x = ", num2str(x)));end % Function
