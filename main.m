02936862ff3c7f7e748d72d39db18e413979a97c
Huldar <huldarh@gmail.com>
2021-04-16 18:08:47 +0000

    Breytt vegna falla
    helmholtzeq.m
    numera_punkta.m

--- main.m
+++ main.m
@@ -1,17 +1,22 @@
 % Fyrri hluti - Helmholtz
 
 % lidur 2 - Prófunarkeyrsla
-lambda = 1/100; L1 = 1; L2 = 1; h = 1/4; w = 1; v = 0;
+lambda = 1/100;
+L1 = 1;
+L2 = 1;
+h = 1./4;
+p = 1;
 
-disp("HZ")
-disp(HZ)
-pit 
+punktar = numera_punkta(0, 0, L1, L2, h);
+
 % Handles fyrir v og w föll
 v_handle = @v_HZ;
 w_handle = @w_HZ;
 
 % Nálga helmholtz fylki
-HZ = helmholtzeq(L1, L2, h, lambda);
+HZ = helmholtzeq(L1, L2, h, lambda, punktar);
+disp("HZ")
+disp(HZ)
 
 
 HZ_lausn = [1. 1. 1. 1. 1.; .75 .75 .75 .75 .75; .50 .50 .50 .50 .50; .25 .25 .25 .25 .25; 0. 0. 0. 0. 0.];
