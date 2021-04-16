64e70fac4af764d856479deb16a321ad48e8a06b
Huldar <huldarh@gmail.com>
2021-04-16 18:07:31 +0000

    Breytt vegna annarra falla
    numera_punkta.m
    helmholtsNalgun.m

--- helmholtzeq.m
+++ helmholtzeq.m
@@ -1,7 +1,7 @@
 % Fall sem tekur inn L1, L2,h og lambda og skilar Helmholtz fylkinu HZ
 % 
-function HZ = helmholtzeq(L1, L2, h, lambda)
-  % 
+function HZ = helmholtzeq(L1, L2, h, lambda, punktar)
+  % Fjöldi bila (ATH ad fjoldi punkta = fjoldi bila + 1
   N = L1/h;
   M = L2/h;
   
@@ -9,10 +9,8 @@ function HZ = helmholtzeq(L1, L2, h, lambda)
   HZ = zeros(M+1 , N+1);
 
   for i = 1:(M+1)   % Rows
-    y = (i-1).*h;
     for j = 1:(N+1) % Columns
-      x = (j-1).*h;
-      HZ(i,j) = helmholtzNalgun(x,y);
+      HZ(i,j) = helmholtzNalgun(punktar, numer_punkts(i,j, N+1), 0, 0, L1, L2, lambda, N+1, M+1, h);
     end    % Column for loop
   end  % Row for loop
 end % Function
