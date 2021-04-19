1fdc8a248c5e4a0e1d617018231db970500e982a
ragnartomasson <ragnartomasson@localhost>
2021-04-19 22:33:50 +0000

    klárar mest allt í liði 2

--- psi1.m
+++ psi1.m
@@ -1,3 +1,3 @@
 function psi_1 = psi1(a,Beta1,x)
-psi_1 = Beta1*(sin(2*pi/a*(x-1/2))+1);
+psi_1 = Beta1*(sin((2*pi)/(a)*(x-(1/2)))+1);
 end
--- varmajafnvaegi.m
+++ varmajafnvaegi.m
@@ -1,9 +1,72 @@
-%forritið fyrir psí verður bara í raun að leysa upphafsgildisverkefnið og
-%svo vera með tvær for loopur sem prenta út í fylki
+function varmajafnvaegi2(a,b,beta1,beta2,h)
+N=a/h;
+M=b/h;
+A=zeros(((a/h)+1)*((b/h)+1),((a/h)+1)*((b/h)+1));
+b=zeros(((a/h)+1)*((b/h)+1));
+M+1
+N+1
+for j=2:N   %innri punktar
+    for k=2:M
+        A(j+(N+1)*(k-1),j+(N+1)*(k-2))=-1;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1)-1)=-1;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1))=4;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1)+1)=-1;
+        A(j+(N+1)*(k-1),j+(N+1)*(k))=-1 ;
+        b(j+(N+1)*(k-1))=0;
+    end
+end
+A;
+
+for j=1:1   %vinstri
+    for k=2:M
+        A(j+(N+1)*(k-1),j+(N+1)*(k-2))=-1/2;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1))=2;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1)+1)=-1;
+        A(j+(N+1)*(k-1),j+(N+1)*(k))=-1/2;
+        b(j+(N+1)*(k-1))=0;
+    end
+end
+A;
+
+for j=N+1:N+1   %hægri
+    for k=2:M
+        A(j+(N+1)*(k-1),j+(N+1)*(k-2))=-1/2;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1)-1)=-1;
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1))=2;
+        A(j+(N+1)*(k-1),j+(N+1)*(k))=-1/2;
+        b(j+(N+1)*(k-1))=0;
+    end
+end
+
+A;
+x=0
+for j=1:N+1   %uppi
+    for k=1:1
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1))=1;
+        b(j+(N+1)*(k-1))=psi1(a,beta1,x);
+        j
+        psi1(a,beta1,j)
+    end
+    x=x+h
+end
+
+A;
+x=0
+for j=1:N+1 %niðri
+
+    for k=M+1:M+1
+        A(j+(N+1)*(k-1),j+(N+1)*(k-1))=1;
+        b(j+(N+1)*(k-1))= psi2(a,beta2,x);
+    end
+    x=x+h
+end
+A;
+b
+A_sparse=sparse(A);
+c=A_sparse\b;
+c=reshape(c,1,[]);
+c=c(1:(N+1)*(M+1))
+c=reshape(c,[N+1,M+1])
+c_transp=c'
 
 
-%testa að psi forritin virki
-Beta1=1; a=1; x=1;Beta2=1;
-psi_1=psi1(a, Beta1, x)
-
-psi_2 = psi2(Beta2,x)
