06d4908baf83e891709598f32985d6eca6864a9e
Huldar <huldarh@gmail.com>
2021-04-20 11:37:53 +0000

    Add generate_A.m
    Fall sem býr til A fylkið

---
+++ generate_A.m
@@ -0,0 +1,14 @@
+% Function sem tekur inn punktagridsvektor punktar
+% x0 er vinstri endapunktur grid
+% y0 er neðri endapunktur grid
+% L1 er hægri endapunktur grid
+% L2 er efri endapunktur grids
+% Lambda er lambda
+% n er fjöldi dálka
+% m er fjöldi raða
+% h er skrefastærð
+
+function A = generate_A(punktar, x0, y0, L1, L2, lambda, n, m, h)
+    A = 3;
+
+end % Function
