69ef54ea106110406ad87c1358566368099cf701
Huldar <huldarh@gmail.com>
2021-04-21 16:00:10 +0000

    fixes #7

--- main.m
+++ main.m
@@ -10,15 +10,19 @@ L2 = 1;
 h = 1/4;
 p = 1;
 
+% handles
+v_handle = @(x, u1,L1) v_HZ(x, u1,L1);
+w_handle = @(x, u0,L1) w_HZ(x, u0,L1);
+
 % Nálga helmholtz fylki
-HZ = helmholtzeq(L1, L2, h, lambda);
-disp("HZ");
-disp(HZ);
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, 0, 0);
+%disp("HZ");
+%disp(HZ);
 
 HZ_lausn = [1. 1. 1. 1. 1.; .75 .75 .75 .75 .75; .50 .50 .50 .50 .50; .25 .25 .25 .25 .25; 0. 0. 0. 0. 0.];
 
-disp("HZ_lausn:");
-disp(HZ_lausn);
+%disp("HZ_lausn:");
+%disp(HZ_lausn);
 
 disp('Ef þetta fylki er nógu nálægt því að vera núll fylkið er helmholtz að virka');
 disp(HZ_lausn-HZ);
@@ -27,17 +31,69 @@ disp(HZ_lausn-HZ);
 L1 = 1;
 L2 = 2;
 h = 1/20;
+[X,Y] = meshgrid(0:h:L1,0:h:L2);
+
 lambda = 1;
+u_fylki = u_HZ(X,Y, lambda, L1, L2);
 
-%u_HZ(x,y, lambda, L1, L2);
-
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,u_fylki, 'FaceAlpha',0.7);
 
 lambda = 10;
+u_fylki = u_HZ(X,Y, lambda, L1, L2);
 
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,u_fylki, 'FaceAlpha',0.7);
 
 % Liður 4 - Teikna graf lausnar
+L1 = 1;
+L2 = 1;
+u0 = 10;
+u1 = 1;
+h = 1/50;
+[X,Y] = meshgrid(0:h:L1,0:h:L2);
 
+% Handles
+v_handle = @(x,u1, L1) v2_HZ(x, u1,L1);
+w_handle = @(x,u0, L1) w2_HZ(x, u0,L1);
 
+% Gera öll lambda-in
+lambda = 1/100;
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, u0, u1);
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,HZ, 'FaceAlpha',0.7);
+title(strcat('Lamda = ',num2str(lambda)));
+
+lambda = 1/10;
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, u0, u1);
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,HZ, 'FaceAlpha',0.7);
+title(strcat('Lamda = ',num2str(lambda)));
+
+lambda = 1;
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, u0, u1);
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,HZ, 'FaceAlpha',0.7);
+title(strcat('Lamda = ',num2str(lambda)));
+
+lambda = 10;
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, u0, u1);
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,HZ, 'FaceAlpha',0.7);
+title(strcat('Lamda = ',num2str(lambda)));
+
+lambda = 30;
+HZ = helmholtzeq(L1, L2, h, lambda, v_handle, w_handle, u0, u1);
+figurecounter = figurecounter+1;
+figure(figurecounter);
+surf(X,Y,HZ, 'FaceAlpha',0.7);
+title(strcat('Lamda = ',num2str(lambda)));
 
 % Seinni hluti - Varmajafnvaegi
 
