1df740734ae13768092bb5a6642df2460109c810
Huldar <huldarh@gmail.com>
2021-04-21 11:42:10 +0000

    Delete hnit_punkts.m

--- hnit_punkts.m
+++
@@ -1,9 +0,0 @@
-% Function sem tekur inn numer punkts í gridinu og skilar út hniti punkts
-% j er númer punkts
-% x0 er vinstri endapunktur grids
-
-function [x,y] = hnit_punkts(j,n)
-    % i er númer dálks, j er númer raðar og n er fjöldi punkta í hverri röð
-    x = 1;
-    y = 1;
-end % function
