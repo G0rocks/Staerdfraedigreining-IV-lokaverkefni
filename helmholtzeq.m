1d85089d75d501181141a02037c2b01e2f8ffd40
Huldar <huldarh@gmail.com>
2021-04-21 11:43:46 +0000

    Del helmholtzNalgun.m
    Gagnslaust, notum generate_A_and_b.m

--- helmholtzeq.m
+++ helmholtzeq.m
@@ -4,13 +4,20 @@ function HZ = helmholtzeq(L1, L2, h, lambda, punktar)
   % Fjöldi bila (ATH ad fjoldi punkta = fjoldi bila + 1
   N = L1/h;
   M = L2/h;
+  n = N+1; %Fjoldi punkta á ás
+  m = M+1; % Fjöldi punkta á ás
   
   % Initialize HZ array with zeros
-  HZ = zeros(M+1 , N+1);
+  HZ = zeros(M+1 , N+1);  
 
-  for i = 1:(M+1)   % Rows
-    for j = 1:(N+1) % Columns
-      HZ(i,j) = helmholtzNalgun(punktar, numer_punkts(i,j, N+1), 0, 0, L1, L2, lambda, N+1, M+1, h);
+  [A,b] = generate_A_and_b(0, lambda, n, m, h);
+  A_inv = inv(A);
+ 
+  C = b.*A_inv;
+  
+  for i = 1:m   % Rows
+    for j = 1:n % Columns
+      HZ(i,j) = C(numer_punkts(i,j,n));
     end    % Column for loop
   end  % Row for loop
 end % Function
